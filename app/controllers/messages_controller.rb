12feba6f52a3c0a08803895b637ee07de65c5b42
itotamami <ito@exmedia.jp>
2015-10-07 08:56:32 +0000

    8.6 After Check Edit Function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,9 @@
 class MessagesController < ApplicationController
+before_action :set_message, only: [:edit, :update]
   # def index
   #end
+  def edit
+  end
 
   ##5.8 メッセージ一覧の表示　で追記
 
@@ -28,12 +31,24 @@ class MessagesController < ApplicationController
     end
 
   end
+  def update
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
 
   private
   def message_params
     params.require(:message).permit(:name, :body)
   end
 
+  def set_message
+    @message = Message.find(params[:id])
+  end
 
 
   ## ここまで
