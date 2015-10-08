b7991cecd9952b8995112f886da3407f9fd76787
itotamami <ito@exmedia.jp>
2015-10-08 08:24:37 +0000

    before review L6_9Task:Add column age

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,5 +4,5 @@ class Message < ActiveRecord::Base
     # 内容は必須入力かつ2文字以上30文字以下
     validates :body , length: {minimum: 2 , maximum: 30 } , presence: true
     # 内容は必須入力かつ2文字以上3文字以下正の整数のみ入力可能
-   # validates :age, length: {minimum: 1 , maximum: 3 } , presence: true,:numericality => {:only_integer => true,greater_than_or_equal_to:0,}
+    validates :age, length: {minimum: 1 , maximum: 3 } , presence: true,:numericality => {:only_integer => true,greater_than_or_equal_to:0,}
 end
