91257730addcec49912f427edde74cfcc7e50bd4
itotamami <ito@exmedia.jp>
2015-10-08 08:03:06 +0000

    L6_9_TASK:not set validate

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -18,7 +18,7 @@ class MessagesController < ApplicationController
 
   ## ここから追記
   def create
-	binding.pry
+#	binding.pry
     @message = Message.new(message_params)
   #  @message.save
   #  redirect_to root_path , notice: 'メッセージを保存しました'
